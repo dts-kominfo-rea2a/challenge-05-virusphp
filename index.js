4687fb064e993e4e4cda677fc27b919142f0fd2d
virusphp <packercyber@gmail.com>
2022-06-05 23:01:00 +0700

    done sort asc and desc

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrNama, cbSort) => {
+  arrData = cbSort(arrNama);
+  let dataSort = [];
+  for(let i = 0; i < arrData.length; i++) {
+    let nama = "";
+    nama += (i+1) +". "+arrData[i];
+    dataSort.push(nama);
+  }
+  return dataSort;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrNama) => {
+  return arrNama.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrNama) => {
+  return arrNama.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
